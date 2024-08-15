22a852d564cdf071f0f3bd67a9e40f0ddce58b8d
Ivan Jobs <ivan1377@163.com>
2024-08-15 11:56:00 +0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,31 @@
+var cookieExipre = new Date();
+cookieExipre.setTime(
+    cookieExipre.getTime() + 10 * 60 * 1000 // Cookie exipre in 10 minutes
+);
+var isSecure = true;
 module.exports = async function (context, req) {
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API" },
+      body: { text: "Hello from the API" },
+      status: 200,
+      cookies: [
+          {
+              name: "cookie1",
+              value: "cookie1",
+              secure: true,
+              httponly: true,
+              path: "/",
+              expires: cookieExipre,
+              sameSite: "None",
+          },
+          {
+              name: "cookie2",
+              value: "cookie2",
+              secure: isSecure,
+              httponly: true,
+              path: "/",
+              expires: cookieExipre,
+              sameSite: "None",
+          },
+      ],
   };
 };
